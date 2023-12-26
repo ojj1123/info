74699be582308338f3f82a82a06e6c354b3ffb82
Jeongjin <rojay.developer@gmail.com>
2023-12-26 18:42:47 +0900

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { Root, Slot } from 'waku/client';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const rootElement = (
   <StrictMode>
