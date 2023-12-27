b27831cc00178361f82dabe9e5704065c8cffc4a
Jeongjin Oh <rojay.developer@gmail.com>
2023-12-27 13:17:18 +0900

    Revert "wip: vanilla extract"

--- src/components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react';
-import { layout } from './Layout.css.js';
-
-export const Layout = ({ children }: PropsWithChildren) => {
-  return <div className={layout}>{children}</div>;
-};
