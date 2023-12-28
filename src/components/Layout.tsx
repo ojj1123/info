aa2ceb6256f735d105bccafa3e75498608c06960
Jeongjin Oh <rojay.developer@gmail.com>
2023-12-29 01:16:06 +0900

    style: use vanilla-extract (#3)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react';
+import { layout } from './Layout.css.js';
+
+export const Layout = ({ children }: PropsWithChildren) => {
+  return <div className={layout}>{children}</div>;
+};
