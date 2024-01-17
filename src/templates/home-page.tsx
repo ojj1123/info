cb1e7bb729b22d99b343f2ea88a86f431a77372b
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-18 01:21:29 +0900

    npm to pnpm

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -1,10 +1,9 @@
 import { Link } from 'waku'
 
 export const HomePage = async () => {
-  const data = await getData()
-
   return (
     <div>
+      <title>Jeongjin</title>
       <h1>I&apos;m Jeongjin Oh</h1>
       <div className='flex flex-col items-start w-full flex-1 text-start pt-5 sm:pt-10 px-0 sm:px-10 lg:px-40'>
         <p></p>
@@ -12,13 +11,4 @@ export const HomePage = async () => {
       <Link to='/about'>About me</Link>
     </div>
   )
-}
-
-const getData = async () => {
-  const data = {
-    title: 'Waku',
-    body: 'Hello world!',
-  }
-
-  return data
 }
