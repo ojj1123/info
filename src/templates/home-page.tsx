dfef27321398ec219cb3591fcaa8e00ecac7d9d0
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-18 00:45:38 +0900

    fix: build error

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'waku'
-import { Description } from '../components/description.js'
 
 export const HomePage = async () => {
   const data = await getData()
 
   return (
     <div>
-      <Description />
+      <h1>I&apos;m Jeongjin Oh</h1>
+      <div className='flex flex-col items-start w-full flex-1 text-start pt-5 sm:pt-10 px-0 sm:px-10 lg:px-40'>
+        <p></p>
+      </div>
       <Link to='/about'>About me</Link>
     </div>
   )
