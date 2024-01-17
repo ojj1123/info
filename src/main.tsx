a13173dd1e4cbec2bd6aa24907324bba20de4fa1
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-18 01:36:31 +0900

    really?

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot, hydrateRoot } from 'react-dom/client'
-import { Root } from 'waku/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { ErrorBoundary } from './components/error-boundary.js'
@@ -8,13 +7,11 @@ import { Router } from 'waku/router/client'
 
 const rootElement = (
   <StrictMode>
-    <Root>
-      <SpeedInsights />
-      <Analytics />
-      <ErrorBoundary fallback={(error) => <h1>{String(error)}</h1>}>
-        <Router />
-      </ErrorBoundary>
-    </Root>
+    <SpeedInsights />
+    <Analytics />
+    <ErrorBoundary fallback={(error) => <h1>{String(error)}</h1>}>
+      <Router />
+    </ErrorBoundary>
   </StrictMode>
 )
 
